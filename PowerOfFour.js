3bcc1f8ccb1cd52a67fa415ef218c1b513e5c950
silver <zhangzuoxuan@gmail.com>
2020-10-20 16:51:47 +0800

    power

--- PowerOfFour.js
+++ PowerOfFour.js
@@ -2,7 +2,7 @@
  * @param {number} num
  * @return {boolean}
  */
-var isPowerOfFour = function(num) {
+var isPowerOfFour = function (num) {
   if ((num & (num - 1)) === 0) {
     while (num > 1) {
       num = num >> 2;
@@ -20,6 +20,3 @@ console.log(isPowerOfFour(5));
 console.log(isPowerOfFour(-4));
 console.log(isPowerOfFour(8));
 console.log(isPowerOfFour(256));
-console.log(isPowerOfFour(32));
-console.log(isPowerOfFour(64));
-console.log(isPowerOfFour(22));
