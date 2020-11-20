60c452c608bc077ffeb6dff6a799f4c1a45ebac4
git <zuoxuanzhang@gmail.com>
2020-11-20 15:53:27 +0800

    3

---
+++ 189. 旋转数组/2.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  const len = nums.length;
+  const rest = nums.splice(len - k);
+  nums.unshift(...rest);
+};
